70ed07868df8f097c17f29cb66d896528b2ca298
danielscoffee <danielrsantoscontato@gmail.com>
2025-12-02 15:20:17 -0300

    feat: part two ended

--- day-1/main.go
+++ day-1/main.go
@@ -43,6 +43,34 @@ func (k *Keyword) Clocking(key []Keyword) int {
 	return zeroCount
 }
 
+func (k *Keyword) ZeroCounters(key []Keyword) int {
+	position := 50
+	zeroCount := 0
+
+	for i := range key {
+		turns := key[i].turns
+		for j := 0; j < turns; j++ {
+			if key[i].direction == L {
+				position--
+				if position < 0 {
+					position = 99
+				}
+			}
+			if key[i].direction == R {
+				position++
+				if position > 99 {
+					position = 0
+				}
+			}
+			if position == 0 {
+				zeroCount++
+			}
+		}
+	}
+
+	return zeroCount
+}
+
 func stringToKeyword(s string) []Keyword {
 	ka := []Keyword{}
 
@@ -72,13 +100,22 @@ func stringToKeyword(s string) []Keyword {
 }
 
 func main() {
-	data, err := os.ReadFile("input.txt")
+	d1, err := os.ReadFile("input_part1.txt")
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
-	keys := stringToKeyword(string(data))
+	d2, err := os.ReadFile("input_part2.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	k1 := stringToKeyword(string(d1))
 	ka := Keyword{}
-	password := ka.Clocking(keys)
-	fmt.Printf("Password: %d\n", password)
+	k2 := stringToKeyword(string(d2))
+
+	password1 := ka.Clocking(k1)
+	fmt.Printf("Password: %d\n", password1)
+	password2 := ka.ZeroCounters(k2)
+	fmt.Printf("Password: %d\n", password2)
 }
