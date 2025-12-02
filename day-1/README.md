53d381a535a7191e8211367a2a6a5e4cbf163315
danielscoffee <danielrsantoscontato@gmail.com>
2025-12-02 01:03:38 -0300

    feat: first day passed

---
+++ day-1/README.md
@@ -0,0 +1,4 @@
+# Day 1 - Advent of Code 2025
+
+- Link
+-> https://adventofcode.com/2025/day/1
---
+++ day-1/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	L = 0
+	R = 1
+)
+
+type Keyword struct {
+	turns     int
+	direction int
+}
+
+func (k *Keyword) Clocking(key []Keyword) int {
+	position := 50
+	zeroCount := 0
+
+	for i := range key {
+		turns := key[i].turns
+		for j := 0; j < turns; j++ {
+			if key[i].direction == L {
+				position--
+				if position < 0 {
+					position = 99
+				}
+			}
+			if key[i].direction == R {
+				position++
+				if position > 99 {
+					position = 0
+				}
+			}
+		}
+		if position == 0 {
+			zeroCount++
+		}
+	}
+
+	return zeroCount
+}
+
+func stringToKeyword(s string) []Keyword {
+	ka := []Keyword{}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == 'L' || s[i] == 'R' {
+			direction := L
+			if s[i] == 'R' {
+				direction = R
+			}
+			i++
+			numStart := i
+			for i < len(s) && s[i] >= '0' && s[i] <= '9' {
+				i++
+			}
+			if numStart < i {
+				num := 0
+				for j := numStart; j < i; j++ {
+					num = num*10 + int(s[j]-'0')
+				}
+				ka = append(ka, Keyword{turns: num, direction: direction})
+			}
+
+			i--
+		}
+	}
+	return ka
+}
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	keys := stringToKeyword(string(data))
+	ka := Keyword{}
+	password := ka.Clocking(keys)
+	fmt.Printf("Password: %d\n", password)
+}
